fc1f1daa4c421228d0a187ebc725358d37cf6b4e
hardik jhalani <hardikjhalani263@gmail.com>
2025-06-09 13:28:29 +0530

    inite commit

---
+++ list.py
@@ -0,0 +1,42 @@
+# question 1
+# numbers = [10, 20, 4, 45, 99]
+# max = numbers[0]
+# for i in ( numbers):
+#    if i>max:
+#     max = i
+# print ("the maxium no is " , max)
+
+#question 2
+# num = [10,20,30,40]
+# sum =0 
+# for i in num:
+#   sum+=i
+# print(sum)
+
+# question 3
+# mylist = [1, 2, 3, 4, 5]
+# n = len(mylist)
+# for i in range(n // 2):
+#     mylist[i], mylist[n - 1 - i] = mylist[n - 1 - i], mylist[i]
+# print(mylist) 
+
+# question 4 
+# numbers = [10, 20, 4, 45, 99]
+# n = len(numbers)
+# for i in range(n):
+#     for j in range(0, n - i - 1):
+#         if numbers[j] > numbers[j + 1]:
+#             numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+# print("Sorted list in ascending order:", numbers)
+
+# question 5
+num = [1, 2, 2, 3, 4, 4, 5 , 6]
+dup = []
+for i in range (len(num)):
+   for j in range (i+1 , len(num)):
+      if num[i] != num[j] and num[i] not in dup:
+         
+         dup.append(num[i])
+print (dup)         
+         
+         
