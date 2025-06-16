9165e99d6557dc6b627d1d82c3ddf74febb5bde2
hardik <hardikjhalani263@gmail.com>
2025-06-16 14:16:17 +0530

    inite commit

---
+++ regex3.py
@@ -0,0 +1,173 @@
+# data = "hardik"
+# while (i>6,):
+#     print (i,data[i])
+#     i+=1
+
+
+# data ="hardik"
+# for i in range (5 , -1,-1 ):
+#     print (i , data[i])
+
+# data = "hardik "
+# i = 0
+# j =5
+# while (i<6):
+#     print(i , data[i] , data[j])
+#     i+=1
+#     j-=1
+
+# data = "tussat"
+# i = 0
+# a = 5
+# count = 0
+# while (i<=a):
+#     if (data[i] == data [a]):
+#         print (i,a)
+#         break
+#     i+=1
+#     a-=1
+# if ( count ==1 ):
+#     print("it is a pelendrom no.")
+# else:
+#     print("is a not pelendrom no. ")  
+
+
+# data = "tussat"
+# i = 0
+# a = 5
+# count = 0
+# while (i<=a):
+#     if (data[i] != data [a]):
+#         print (i,a)
+#         count = 1
+#         break
+#     i+=1
+#     a-=1
+# if ( count ==1 ):
+#     print("it is a pelendrom no.")
+# else:
+#     print("is a not pelendrom no. ")         
+
+
+# data = "regexsoftware"
+# i = 0 
+# a = 6
+# while (i<a):
+#     print (data[i])
+#     i+=1
+
+
+# data = "regexsregex"
+# i = len(data)//2+1
+# j = 0
+# while (i< len (data )):
+#     print (data[j],data[i] , i,j)
+#     i+=1
+#     j+=1    
+
+# for i in range (10 ,0 ,-1):
+#     print (" ----> python student",i)
+
+#     for j in range (1,9):
+#         print ("assiment ",j)
+
+# for i in range (1,9):
+#     print ("-----> python student ")
+
+#     for j in range (1,7):
+#         print("assign => ", j, end = " ")
+
+#     print (" ")     
+
+# for i in range (5 ,0 ,-1):
+#     print (" ----> python student " , i,"assignment: ", end=" ")
+
+#     for j in range (i , 0,-1 ):
+#         print (j , end = " ")
+#     print (" ")  
+
+# for i in range (1,4):
+#     print ()
+
+#     for j in range (1,4):
+#         print("*", end = "")
+#     print ("")     
+
+# for i in range (1,5):
+#     print ()
+
+#     for j in range (1,i+1):
+#         print("*", end ="")
+    
+# for i in range (1,5):
+
+#     for j in range (1,4):
+#         print(i, end = " ")     
+#     print (' ')
+
+# for i in range (1,5):
+
+#     for j in range (1,4):
+#         print(j, end = " ")     
+#     print (' ')
+
+# for i in range (5,0 , -1):
+
+#     for j in range (1,4):
+#         print(i, end = " ")     
+#     print (' ')
+
+# for i in range (1,5):
+#     a = 65
+#     for j in range (1,5):
+#         print(chr(a), end = " ") 
+#         a+=1    
+#     print (' ')
+
+# for i in range (1,5):
+
+#     for j in range (7,10):
+#         print(j, end = " ")     
+#     print (' ')
+
+# a =1
+# for i in range ( 1 ,5):
+#     for j in range ( 1,i+1 ):
+#         print ( a , end = " ")
+#     print ( )    
+
+# for i in range ( 1 ,5):
+#     a = 1
+#     for j in range ( 1,i+1 ):
+#         print ( a , end = " ")
+#         a+=1
+#     print ( )      
+
+# for i in range ( 5 , 0 , -1):
+#     a = 4
+#     for j in range ( 1,i+1 ):
+#         print ( a , end = " ")
+#         a -=1
+#     print ( )  
+
+# for i in range ( 5 , 0 , -1):
+#     a = 65
+#     for j in range ( 1,i+1 ):
+#         print ( chr(a) , end = " ")
+#         a +=1
+#     print ( )    
+
+# a =65
+# for i in range (1,5):
+#     for j in range ( 1,i+1 ):
+#         print ( chr(a) , end = " ")
+#         a +=1
+#     print ( )          
+
+for i in range(1,5):
+ 
+   for j in range(4,i-1,-1):
+    print("-", end = "  ")  
+   # print("*", end = " ")     
+    print (' ')
+
