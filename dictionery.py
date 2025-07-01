c14fbc092b8d5587de05d8afc328023829431b26
hardik jhalani <hardikjhalani263@gmail.com>
2025-07-01 10:59:14 +0530

    inite commit

---
+++ dictionery.py
@@ -0,0 +1,39 @@
+# myAdhar = {10:"yash" , 11:"happy"}
+# print (myAdhar[11])    # access[key]
+
+# myAdhar [11] = "sad"        #update
+# myAdhar [19] = "kajal"      # insert
+# print(myAdhar)
+
+# myAdhar = {10:"yash" , 11:"happy" , "salary": 80000}
+# myAdhar.pop(11)
+# print (myAdhar)
+
+# myAdhar ={"salary": 58000}
+# myAdhar ["salary"] = myAdhar["salary"] + 1000
+# print (myAdhar)
+
+# dict ={"total":0}
+# for i in range (1,19):
+#   dict ["dict"] = dict ["total"] +1
+#   print (dict)  
+
+# data = "hard"
+# mydict ={"count":0}
+# for i in data:
+#   print(i)
+#   mydict["count"]+1
+
+# a = "abcdefghijk"
+# dict = {"total":10}
+# count = 0
+# for i in a:
+#   if (i== 'a' or i == 'o' or i == 'u' or i == 'e' or i == 'i' ):
+#     count +=1
+#     dict["total"] = count
+#     print(dict)
+
+data = {}
+for i in range (0,6):
+ data[i] = i*i
+print (data)    
