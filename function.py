0f7964587e48825620c4c338d9d065c103b9e9e1
hardik jhalani <hardikjhalani263@gmail.com>
2025-07-05 09:50:48 +0530

    inite commit

---
+++ function.py
@@ -0,0 +1,17 @@
+def even_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is Even")
+    else:
+        print(f"{number} is Odd")
+
+
+
+def area_of_triangle(base, height):
+    area = 0.5 * base * height
+    print(f"Area of triangle is {area}")
+
+
+
+def volume_of_cuboid(length, width, height):
+    volume = length * width * height
+    print(f"Volume of cuboid is {volume}")
