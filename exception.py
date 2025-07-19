560ae869b3c97a8af92bd14c51dce0a5be5df3ce
hardik jhalani <hardikjhalani263@gmail.com>
2025-07-19 08:59:48 +0530

    inite commit

--- exception.py
+++ exception.py
@@ -37,17 +37,36 @@
 #     else:
 #         break
 
-while True:
-    try:
-        num1 = int(input("enter first value: "))
-        num2 = int(input("enter second value: "))
-        print(num1/num2)
-    except ZeroDivisionError:
-        print("beta zero se divide mt kro") 
-    except ValueError:
-        print("error")       
-    except Exception as e:
-        print("error tumhare......" , e)
-    else:
-        break     
+# while True:
+#     try:
+#         num1 = int(input("enter first value: "))
+#         num2 = int(input("enter second value: "))
+#         print(num1/num2)
+#     except ZeroDivisionError:
+#         print("beta zero se divide mt kro") 
+#     except ValueError:
+#         print("beta value se divide mt kro")       
+#     except Exception as e:
+#         print("error tumhare......" , e)
+#     else:
+#         break     
        
+
+# count = 1
+# while count >1:
+#     try:
+#         num1 = int(input("enter first value: "))
+#         num2 = int(input("enter second value: "))
+#         print(num1/num2)
+#     except ZeroDivisionError:
+#         count +1
+#     except ValueError:
+#         count +1   
+#     except Exception as e:
+#         count +1
+#     else:
+#         break     
+
+x = int(input())
+y = int(input())
+print(x-y)
